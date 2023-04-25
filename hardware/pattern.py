e16f9652a44487bdadcd611fc6d37ccf9a0fe3dc
Jiwon Choi <cjwon531@gmail.com>
2023-04-25 16:39:10 -0700

    chessboard generator refactored

--- hardware/pattern.py
+++ hardware/pattern.py
@@ -1,4 +1,49 @@
 import numpy as np
+import cv2
+
+def generate_chessboard(resolution: tuple, checker_pixel: int, num_col: int, num_row: int):
+    '''
+    Generate chessboard for geometric calibration -> generate chessboard image to display on the screen
+    
+    Parameters:
+        @resolution: monitor resolution
+        @checker_pixel: pixel dimension of a single checker
+        @num_row: chessboard number of rows
+        @num_col: chessboard number of columns
+    '''
+
+    # grab resolution
+    width = resolution[0]
+    height = resolution[1]
+
+    # exception handling (invalid inputs)
+    if checker_pixel > min(width, height):
+        raise Exception('Single checker pixel dimension is larger than the width/height of the monitor!')
+    elif (num_col * checker_pixel) > width:
+        raise Exception('Number of columns exceeds the board dimensions!')
+    elif (num_row * checker_pixel) > height:
+        raise Exception('Number of rows exceeds the board dimensions!')
+
+    checker_width = num_col * checker_pixel
+    checker_height = num_row * checker_pixel
+
+    # find chessboard drawing starting point
+    pading_x = (width - checker_width) // 2 # calculate width offset
+    pading_y = (height - checker_height) // 2 # calculate height offset
+
+    chessboard = np.full((height, width), 255, dtype=np.uint8)
+    white = False
+    for i in range(num_row):
+        for j in range(num_col):
+            x1, y1 = j * checker_pixel + pading_x, i * checker_pixel + pading_y
+            x2, y2 = x1 + checker_pixel, y1 + checker_pixel
+            if not white:
+                chessboard[y1:y2, x1:x2] = 0
+            white = not white
+        white = not white
+    
+    cv2.imwrite('./data/geometric_chessboard.png', chessboard)
+    
 
 class GradientPattern:
     def __init__(self, resolution, nph=2, frequency=0):
