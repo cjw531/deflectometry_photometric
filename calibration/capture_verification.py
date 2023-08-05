91150ec060aed3733afeb000961b3c7e77c97726
Jiwon Choi <cjwon531@gmail.com>
2023-08-05 18:53:34 +0900

    overexposure verify check all 'period_*' subfolders

--- calibration/capture_verification.py
+++ calibration/capture_verification.py
@@ -26,25 +26,30 @@ def verify_overexposure(img_folder, filename=None, img_extension='*.tif'):
     Multiple ways to check the potential overexposure
     
     Parameters:
-        @img_folder: captured data folder
+        @img_folder: captured data folder (has subfolders of period_*)
         @filename: specific file name, if not set then arbitrarily selected for plot
         @img_extension: image file type, if not set then by default *.tif format
     '''
 
-    # if specific filename not provided, arbitrarily select one
-    if filename is None: 
-        file_list = sorted(glob.glob(os.path.join(img_folder, img_extension)))
-        filename = file_list[0]
-    else:
-        filename = os.path.join(img_folder, filename)
+    subfolder_count = len(glob.glob(os.path.join(img_folder, 'period_*'))) # sub-folders, period_*
 
-    max_pixel_value(img_folder, img_extension)
-    fig, ax = plt.subplots(1, 2, figsize=(12, 5))
-    ax_pix = pixel_distribution(filename, ax) # pixel distribution (0-255) histogram plot
-    ax = ax_pix
-    ax_red = mark_overexposure(filename, ax) # mark 255 values in red color
-    ax = ax_red
-    plt.show()
+    for i in range(subfolder_count):
+        folder_period = os.path.join(img_folder, 'period_' + str(2 ** i)) # get subfolder
+        
+        # if specific filename not provided, arbitrarily select one
+        if filename is None: 
+            file_list = sorted(glob.glob(os.path.join(folder_period, img_extension)))
+            file_plot = file_list[0]
+        else:
+            file_plot = os.path.join(folder_period, filename)
+
+        max_pixel_value(folder_period, img_extension)
+        fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+        ax_pix = pixel_distribution(file_plot, ax) # pixel distribution (0-255) histogram plot
+        ax = ax_pix
+        ax_red = mark_overexposure(file_plot, ax) # mark 255 values in red color
+        ax = ax_red
+        plt.show()
 
 def max_pixel_value(img_folder, img_extension):
     '''
